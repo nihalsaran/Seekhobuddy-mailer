ce3e4d561bfac3f9f626a5e7a5f1943708006c98
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-07-31 01:12:46 +0530

    cors update

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
