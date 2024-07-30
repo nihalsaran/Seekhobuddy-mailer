5d8a526dc38097f005d8b2bbe41eb0bfe62530e3
Nihal saran das duggirala <92871743+nihalsaran@users.noreply.github.com>
2024-07-31 01:18:57 +0530

    corrsss

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,7 +5,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
